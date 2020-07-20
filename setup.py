ec86695f3cced007d8c9a9522d5b70bd80617045
alfreedolf <alfreedolf@users.noreply.github.com>
2020-07-20 02:13:37 +0200

    minor edit to docstring

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='radix-repr-var',
-      version='0.1',
+      version="0.1",
       description='Radix Representations Variations',
       packages=['radix_repr_var'],
       zip_safe=False)
